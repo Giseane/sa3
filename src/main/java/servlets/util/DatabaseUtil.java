eb90271128af0a42c18a75f1bf1868f5afe9c99a
Giseane <giseane_macedo@estudante.sesisenais.org>
2023-08-08 23:48:36 -0300

    Alterações projeto

--- src/main/java/servlets/util/DatabaseUtil.java
+++ src/main/java/servlets/util/DatabaseUtil.java
@@ -5,17 +5,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseUtil {
-	//mysql 
-	//String URL = "jdbc:Mysql://localhost:3306/seu_banco_de_dados";
-	// Class = com.mysql.cj.jdbc.Driver
 	
-    private static final String URL = "jdbc:postgresql://localhost:5432/teste_new";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "91095850";
+//    private static final String URL = "jdbc:postgresql://localhost:5432/teste_new";
+//    private static final String USER = "postgres";
+//    private static final String PASSWORD = "91095850";
+	
+  private static final String URL = "jdbc:Mysql://localhost:3306/cadastro";
+  private static final String USER = "root";
+  private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         try {
-            Class.forName("org.postgresql.Driver");
+//            Class.forName("org.postgresql.Driver");
+        	Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
